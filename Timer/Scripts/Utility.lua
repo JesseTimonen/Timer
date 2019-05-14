5bb3dded213fa36d93d04dff9cc27f8de2222511
Jesse <47982943+JesseTimonen@users.noreply.github.com>
2019-05-14 13:31:21 +0300

    Update 1.0.2

--- Timer/Scripts/Utility.lua
+++ Timer/Scripts/Utility.lua
@@ -1,17 +1,17 @@
 function getTime(totalTime)
-    -- Get hours, minutes, seconds and milliseconds from total time --
-    local hours = totalTime / 3600;
-    totalTime = totalTime % 3600;
-    local minutes = totalTime / 60;
-    local seconds = totalTime % 60;
-    local milliseconds = (totalTime % 1) * 1000;
+	-- Get hours, minutes, seconds and milliseconds from total time --
+	local hours = totalTime / 3600;
+	totalTime = totalTime % 3600;
+	local minutes = totalTime / 60;
+	local seconds = totalTime % 60;
+	local milliseconds = (totalTime % 1) * 1000;
 
-    -- Return parsed time --
+	-- Return parsed time --
 	time = {}
 	time["hours"] = hours;
 	time["minutes"] = minutes;
-    time["seconds"] = seconds;
-    time["milliseconds"] = milliseconds;
+	time["seconds"] = seconds;
+	time["milliseconds"] = milliseconds;
 	return time;
 end
 
@@ -21,13 +21,13 @@ function getLenght(array)
 	-- Check is array empty --
 	if array == nil then
 		return 0;
-    end
-    
-    -- Get size --
+	end
+
+	-- Get size --
 	local count = 0;
 	for a, b in pairs(array) do
 		count = count + 1;
-    end
-    
+	end
+
 	return count;
 end
